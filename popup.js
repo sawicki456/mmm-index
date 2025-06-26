be1df6a3c2c2c1157aecaa9b561e46f1c714477c
Bigdumbmedia <114912182+sawicki456@users.noreply.github.com>
2025-06-26 07:11:05 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,39 +1,32 @@
 const CLIENT_ID = '705073291364-vepbigh687lpkg1qjosupk9961nbnagd.apps.googleusercontent.com';
 const API_KEY = 'AIzaSyCbNx0M5dMLY-51grw45LJRLjfC_f8eniA';
-const ROOT_FOLDER_ID = '135BPvCXeRAXDOEMAlenWzoicRmtn_e_7'; // Your main project folder
-
+const ROOT_FOLDER_ID = '135BPvCXeRAXDOEMAlenWzoicRmtn_e_7';
 const SCOPE = ['https://www.googleapis.com/auth/drive.file'];
 
 let oauthToken = null;
 
-// Button click handler for upload
+// Upload button triggers Google Picker
 document.getElementById('upload-btn').onclick = function() {
   document.getElementById('status').innerText = "Loading Google Picker...";
-  // Load APIs
-  gapi.load('client:auth2', onAuthApiLoad);
+  gapi.load('client:auth2', initAuth);
   gapi.load('picker', onPickerApiLoad);
 };
 
-function onAuthApiLoad() {
+function initAuth() {
   gapi.auth2.init({
     client_id: CLIENT_ID,
     scope: SCOPE.join(' ')
-  }).then(function() {
-    // Sign in (prompts user if not already)
-    gapi.auth2.getAuthInstance().signIn().then(function(user) {
+  }).then(() => {
+    gapi.auth2.getAuthInstance().signIn().then((user) => {
       oauthToken = user.getAuthResponse().access_token;
-      if (window.pickerApiLoaded) {
-        showPicker();
-      }
+      if (window.pickerApiLoaded) showPicker();
     });
   });
 }
 
 function onPickerApiLoad() {
   window.pickerApiLoaded = true;
-  if (oauthToken) {
-    showPicker();
-  }
+  if (oauthToken) showPicker();
 }
 
 function showPicker() {
